a75d92a8efa83145590107550b4493d9d6d11079
singireddysriram <ssriram8790@gmail.com>
2025-01-31 18:29:24 +0530

    removed backend root directory

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`);
 });
 
-app.use('/home', (req, res) => {
+app.use('/', (req, res) => {
     res.send("<h1> Welcome to SUBY");
 });
